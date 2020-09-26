8524d6d2286986bde096e0af88a53e14d8d656f9
yunus-atmaca <yunusatmacabm@gmail.com>
2020-09-26 10:47:43 +0300

    inital

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { View } from 'react-native'
+
+import Pager from './src/Pager'
+
+const App = () => {
+  return (
+    <View style={{ flex: 1 }}>
+      <Pager>
+
+      </Pager>
+    </View>
+  )
+}
+
+export default App
---
+++ src/Pager.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { View, Dimensions } from 'react-native'
+
+interface PagerProps {
+
+}
+
+class Pager extends React.Component<PagerProps, any>{
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+
+      </View>
+    )
+  }
+}
+
+export default Pager

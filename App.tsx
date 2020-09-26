27eff3c2faa08193fc86f2e9d0e0c5fe1b8bc4f8
yunus-atmaca <yunusatmacabm@gmail.com>
2020-09-26 19:22:48 +0300

    implement basic pager

--- App.tsx
+++ App.tsx
@@ -1,15 +1,68 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, Dimensions, Text } from 'react-native'
 
 import Pager from './src/Pager'
+
+const { width, height } = Dimensions.get('window')
+const data = [
+  {
+    title: '1'
+  },
+  {
+    title: '2'
+  },
+  {
+    title: '3'
+  },
+  {
+    title: '4'
+  },
+  {
+    title: '5'
+  },
+  {
+    title: '6'
+  },
+  {
+    title: '7'
+  },
+]
 
 const App = () => {
   return (
     <View style={{ flex: 1 }}>
-      <Pager>
-
+      <Pager
+        horizontal={false}
+        width={width}
+        height={512}
+        initialIndex={3}
+        showsVerticalScrollIndicator={false}
+        showsHorizontalScrollIndicator={false}>
+        {
+          data.map((item, index) => {
+            return (
+              <View
+                key={'key' + index}
+                style={{
+                  height: 512,
+                  width: width,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  backgroundColor: 'red',
+                }}>
+                <Text style={{
+                  color: 'white',
+                  fontSize: 21,
+                  fontWeight: 'bold'
+                }}>
+                  {item.title}
+                </Text>
+              </View>
+            )
+          })
+        }
       </Pager>
-    </View>
+    </View >
   )
 }
 
--- src/Pager.tsx
+++ src/Pager.tsx
@@ -1,22 +1,172 @@
 import React from 'react'
-import { View, Dimensions } from 'react-native'
+import { View, Dimensions, StyleSheet, ScrollView } from 'react-native'
+import PropTypes from 'prop-types'
+
+const { width, height } = Dimensions.get('window')
+
+const MIN_DISTANCE_FOR_FLING = 50
 
 interface PagerProps {
-
+  horizontal: boolean
+  width?: number,
+  height?: number,
+  initialIndex?: number,
+  showsHorizontalScrollIndicator?: boolean,
+  showsVerticalScrollIndicator?: boolean,
 }
 
 class Pager extends React.Component<PagerProps, any>{
+  scrollViewRef: any
+  isScrolling: boolean
+
+  lastMotion: number
+  selectedPage: number
+
   constructor(props) {
     super(props)
+
+    this.state = this._initialState()
+
+    this.isScrolling = false
+    this.selectedPage = this.state.initialIndex
+    this.lastMotion = 0
+
+  }
+
+  _initialState = () => {
+
+    let initialState = {
+      width: this.props.width || width,
+      height: this.props.height || height,
+      horizontal: this.props.horizontal,
+      initialIndex: this.props.initialIndex || 0
+    }
+
+    return initialState
+  }
+
+  componentDidMount() {
+    //console.debug('componentDidMount')
+
+  }
+
+  componentWillUnmount() {
+    //console.debug('componentWillUnmount')
+
+  }
+
+  componentDidUpdate(prevProps) {
+    console.debug('componentDidUpdate')
+
+    if (this.props !== prevProps) {
+      this.setState({ ...this.props }, () => {
+        this.isScrolling = false
+        this.selectedPage = this.state.initialIndex
+        this.lastMotion = 0
+
+        this._scrollToIndex(this.selectedPage, false)
+      })
+    }
+  }
+
+  _onScroll = (e) => {
+    //console.debug('_onScroll')
+    //console.log(e.nativeEvent.contentOffset)
+  }
+
+  _onScrollBeginDrag = (e) => {
+    this.isScrolling = true
+    //console.debug('_onScrollBeginDrag')
+
+    //this.lastMotion = e.nativeEvent.contentOffset.
+
+    this.lastMotion = this.state.horizontal ?
+      e.nativeEvent.contentOffset.x : e.nativeEvent.contentOffset.y
+  }
+
+  _onScrollEndDrag = (e) => {
+    this.isScrolling = false
+
+    //console.log(e.nativeEvent.contentOffset)
+    //console.log(this.state.horizontal)
+    let motion = this.state.horizontal ?
+      (e.nativeEvent.contentOffset.x - this.lastMotion)
+      :
+      (e.nativeEvent.contentOffset.y - this.lastMotion)
+
+    this._computePagerOffset(motion)
+  }
+
+  _onMomentumScrollEnd = (e) => {
+    console.log('_onMomentumScrollEnd')
+  }
+
+  _computePagerOffset = (motion) => {
+    //console.debug('_computePagerOffset')
+    //console.log(motion)
+    let isPageChanged = false
+    if (Math.abs(motion) > MIN_DISTANCE_FOR_FLING) {
+      this.selectedPage = motion > 0 ?
+        (this.selectedPage + 1)
+        :
+        (this.selectedPage - 1)
+
+      isPageChanged = true
+    }
+
+    let x = this.state.horizontal ? this.state.width * this.selectedPage : 0
+    let y = this.state.horizontal ? 0 : this.state.height * this.selectedPage
+
+    this.scrollViewRef.scrollTo({
+      x: x,
+      y: y,
+      animated: true
+    })
+
+    if (isPageChanged) {
+      this._onPageSelected(this.selectedPage)
+    }
+  }
+
+  _scrollToIndex = (index, animated) => {
+    let x = this.state.horizontal ? this.state.width * index : 0
+    let y = this.state.horizontal ? 0 : this.state.height * index
+
+    this.scrollViewRef.scrollTo({
+      x: x,
+      y: y,
+      animated: animated
+    })
+  }
+  _onPageSelected = (page) => {
+    //console.debug('Selected Page: ', page)
   }
 
   render() {
     return (
-      <View style={{ flex: 1 }}>
-
+      <View style={[styles.container,
+      {
+        width: this.state.width,
+        height: this.state.height
+      }]}>
+        <ScrollView
+          ref={ref => this.scrollViewRef = ref}
+          {...this.props}
+          onScroll={this._onScroll}
+          onScrollBeginDrag={this._onScrollBeginDrag}
+          onScrollEndDrag={this._onScrollEndDrag}>
+          {this.props.children}
+        </ScrollView>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+})
 
 export default Pager
